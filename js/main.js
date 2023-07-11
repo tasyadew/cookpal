36666e06e9c640e1d42a9c149c7520a7b54004e0
Natasya Nadhira <natasyanadhira51@gmail.com>
2023-07-12 02:27:16 +0800

    rework search and filter

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var originalRecipeCard = $("#recipe .card");
+  
+    for (var i = 0; i < 3; i++) {
+      var clonedCard = originalRecipeCard.clone();
+      $(".card-container").append(clonedCard);
+    }
+
+  });
