e98f91b74296a23465fd4a2cacd210d01e35104a
Natasya Nadhira <natasyanadhira51@gmail.com>
2023-07-12 23:26:06 +0800

    create bookmark page

---
+++ pages/bookmark.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CookPAL</title>
+    <link rel="icon" href="../assets/cookpal.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <nav id="navbar">
+        <div class="nav-container">
+            <div class="left-items">
+                <a href="../index.php"><img src="../assets/cookpal.png" class="logo"></a>
+                <div id="saved-recipe" class="navbar-item">Saved Recipe</div>
+            </div>
+            <div class="right-items">
+                <div id="welcome-message" class="navbar-item">Welcome</div>
+            </div>
+        </div>
+    </nav>
+
+    <section id="bookmark">
+        <div class="bookmark-container">
+            <h1>Your Saved Recipe</h1>
+        </div>
+    </section>
+
+    <footer>
+        <div>
+            <p>© CookPAL 2023. All Rights Reserved</p>
+        </div>
+    </footer>
+</body>
+</html>
